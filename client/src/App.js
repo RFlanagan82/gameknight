951387bda19dd96d78fd6fb5479e6b09f4c5dc5d
Aubrey Polk <aubstacle3@gmail.com>
2020-11-06 14:33:07 -0500

    Navbar Component

--- client/src/App.js
+++ client/src/App.js
@@ -1,34 +1,26 @@
 import { useEffect, useState } from "react";
 import Axios from "axios";
+import Navbar from "./components/Navbar/Navbar";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
 
 function App() {
-
   useEffect(() => {
-      console.log("Make an API call");
-      Axios.get("/api/config")
+    console.log("Make an API call");
+    Axios.get("/api/config")
       .then((response) => {
         console.log(response);
-      }).catch((err) => {
-        console.log(err)
-    });
-  },[]);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
 
   return (
     <div className="App">
-      <header className="App-header">
-
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Navbar />
+      </Router>
     </div>
   );
 }
---
+++ client/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+import { Link, NavLink } from "react-router-dom";
+import "./Navbar.css";
+
+const Navbar = () => {
+  return (
+    <nav className="navbar navbar-expand-lg">
+      <Link to="/" className="navbar-brand">
+        GameKnight
+      </Link>
+      <button
+        className="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarNavAltMarkup"
+        aria-controls="navbarNavAltMarkup"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span className="navbar-toggler-icon"></span>
+      </button>
+      <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+        <div className="navbar-nav ml-auto">
+          <NavLink to="/events" className="nav-link">
+            Events
+          </NavLink>
+          <NavLink to="/profile" className="nav-link">
+            Profile
+          </NavLink>
+          <NavLink to="/login" className="nav-link">
+            Login
+          </NavLink>
+          <NavLink to="/about" className="nav-link">
+            About
+          </NavLink>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
