bc38a75fd3a3b8056754f4fa2992a3f4fb9400b7
Ryan Flanagan <flanagan.ryan@gmail.com>
2020-11-06 11:21:49 -0500

    Add build folder static and route to server.js

--- server.js
+++ server.js
@@ -7,8 +7,14 @@ const PORT = process.env.PORT || 3001;
 
 const app = express();
 
+app.use(express.static("client/build"));
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'));
+  }
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/gameknight", {
   useNewUrlParser: true,
@@ -32,6 +38,10 @@ app.get("/api/config", (req, res) => {
     success: true,
   });
 });
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "./client/build/index.html"));
+  });
 
 app.listen(PORT, () => {
   console.log(`App is running on http://localhost:${PORT}`);
