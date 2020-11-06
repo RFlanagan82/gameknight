537d1abc76d0974fc9f40a7de466f150e153e00f
ACE <67081507+aimeecesler@users.noreply.github.com>
2020-11-06 13:50:17 -0500

    added router paths and base skeletons for each page

---
+++ client/src/pages/CreateEditEvent/CreateEditEvent.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const CreateEditEvent = () => {
+    return (
+        <div>
+            <h1>CREATE/EDIT EVENT PAGE</h1>
+        </div>
+    );
+};
+
+export default CreateEditEvent;
