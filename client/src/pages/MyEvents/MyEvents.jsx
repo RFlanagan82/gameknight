f0b761cb6a1a3c85285dff49382d90705a421d25
TDWalsh <thomas.walsh736@gmail.com>
2020-11-06 23:04:36 -0500

    put the code into the correct jsx file

--- client/src/pages/MyEvents/MyEvents.jsx
+++ client/src/pages/MyEvents/MyEvents.jsx
@@ -1,55 +1,9 @@
-import React, { useState, useEffect } from "react";
-import axios from "axios";
-import Container from "../../components/Container/Container";
-import Row from "../../components/Row/Row";
-
-function Events() {
-  const [events, setEvents] = useState([]);
-
-  useEffect(() => {
-    loadEvents();
-  }, []);
-
-  const getEvents = function () {
-    return axios.get("/api/events");
-  };
-
-  function loadEvents() {
-    getEvents()
-      .then((res) => setEvents(res.data))
-      .catch((err) => console.log(err));
-  }
-
-  return (
-    <>
-      {events.map((eventaroo) => (
-        <Container>
-          <Row>
-            <h1>Event Name: {eventaroo.eventName}</h1>
-          </Row>
-          <Row>
-            <h2>Event Date: {eventaroo.date}</h2>
-          </Row>
-          <Row>
-            <h3>Game: {eventaroo.gameName}</h3>
-          </Row>
-          <Row>
-            <h4>Category: {eventaroo.gameCategory}</h4>
-          </Row>
-          <Row>
-            <h5>Event Description: {eventaroo.description}</h5>
-          </Row>
-          <Row>
-            <h6>Max number of players: {eventaroo.maxAttendees}</h6>
-          </Row>
-          <Row>
-          <a href={eventaroo.eventLink}>{eventaroo.eventLink}</a>
-          </Row>
-          
-        </Container>
-      ))}
-    </>
-  );
-}
-
-export default Events;
+import React from 'react';
+const MyEvents = () => {
+    return (
+        <div>
+            <h1>MY EVENTS PAGE</h1>
+        </div>
+    );
+};
+export default MyEvents;
