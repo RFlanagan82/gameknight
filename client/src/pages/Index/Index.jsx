074b25aae0861cc3239fce79be0fccda1665702c
ACE <67081507+aimeecesler@users.noreply.github.com>
2020-11-08 22:14:02 -0500

    added basic structure for home page

--- client/src/pages/Index/Index.jsx
+++ client/src/pages/Index/Index.jsx
@@ -1,11 +1,60 @@
-import React from 'react';
+import React from "react";
+import { Link } from "react-router-dom";
+import Container from "../../components/Container/Container";
+import Row from "../../components/Row/Row";
+import HomeJumbotron from "../../components/HomeJumbotron/HomeJumbotron";
+import "./Index.css";
+import browseImg from "../../images/gamenight1.jpg";
+import hostImg from "../../images/gamenight2.jpg";
 
 const Index = () => {
-    return (
-        <div>
-            <h1>HOME PAGE</h1>
-        </div>
-    );
+  return (
+    <>
+      <HomeJumbotron />
+      <Container>
+        <Row>
+          <div className="col-sm-12 text-center my-5">
+            <h1>Stuck at home and bored?</h1>
+            <h1>Don't make it a lame night, make it a Game Knight!</h1>
+          </div>
+        </Row>
+        <Row>
+          <div className="col-sm-6 text-center mb-5">
+            <Link to="/login">
+              <div className="card bg-dark">
+                <img src={hostImg} className="card-img" alt="monopoly" />
+                <div className="card-img-overlay">
+                  <div className="game-text">
+                    <h5 className="card-title">HOST GAME</h5>
+                    <p className="card-text">
+                      Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                      Debitis.
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </Link>
+          </div>
+          <div className="col-sm-6 text-center mb-5">
+            <Link to="/events">
+              <div className="card bg-dark">
+                <img src={browseImg} className="card-img" alt="monopoly" />
+                <div className="card-img-overlay">
+                  <div className="game-text">
+                    <h5 className="card-title">BROWSE GAMES</h5>
+                    <p className="card-text">
+                      Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                      Debitis.
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </Link>
+          </div>
+        </Row>
+      </Container>
+    </>
+  );
 };
 
 export default Index;
