7797a216009cba16a2feed22f117640dd86a353a
ACE <67081507+aimeecesler@users.noreply.github.com>
2020-11-10 12:41:30 -0500

    added user authentication/authorization and stored token as a jwt. Locked down create event route so that it can only be done by a logged in user and added that user token as the host id

---
+++ controllers/authController.js
@@ -0,0 +1,105 @@
+const express = require("express");
+const router = express.Router();
+const db = require("../models");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+
+// Sign up
+router.post("/api/signup", (req, res) => {
+  const {
+    userName,
+    ageRange,
+    bio,
+    email,
+    password,
+    image,
+    location,
+  } = req.body;
+  if (!email.trim() || !password.trim()) {
+    res.status(400);
+  } else {
+    bcrypt
+      .hash(password, 10)
+      .then((hashedPassword) => {
+        db.User.create({
+          userName: userName,
+          ageRange: ageRange,
+          bio: bio,
+          email: email,
+          password: hashedPassword,
+          image: image,
+          location: location,
+        })
+          .then((NewUser) => {
+            const token = jwt.sign({ userId: NewUser._id }, process.env.SECRET);
+            res.json({
+              error: false,
+              data: token,
+              message: "Successfully signed up.",
+            });
+          })
+          .catch((err) => {
+            console.log(err);
+            res.status(500).json({
+              error: true,
+              data: null,
+              message: "Failed to add new user.",
+            });
+          });
+      })
+      .catch((err) => {
+        console.log(err);
+        res.status(500);
+      });
+  }
+});
+
+// Login
+router.post("/api/login", (req, res) => {
+  const { email, password } = req.body;
+  db.User.findOne({ email: email })
+    .then((foundUser) => {
+      if (foundUser) {
+        bcrypt
+          .compare(password, foundUser.password)
+          .then(function (result) {
+            console.log(result);
+            if (result) {
+              const token = jwt.sign(
+                { userId: foundUser._id },
+                process.env.SECRET
+              );
+              res.json({
+                error: false,
+                data: token,
+                message: "Successfully logged in.",
+              });
+            } else {
+              res.status(401).json({
+                error: true,
+                data: null,
+                message: "Failed to sign in.",
+              });
+            }
+          })
+          .catch((err) => {
+            console.log(err);
+            res.status(401).json({
+              error: true,
+              data: null,
+              message: "Failed to sign in.",
+            });
+          });
+      }
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({
+        error: true,
+        data: null,
+        message: "Failed to sign in.",
+      });
+    });
+});
+
+module.exports = router;
--- controllers/eventController.js
+++ controllers/eventController.js
@@ -1,10 +1,9 @@
 const express = require("express");
-
+const jwt = require("jsonwebtoken");
 const router = express.Router();
 const db = require("../models");
 
 router.get("/", (req, res) => {
-
   db.Event.find({})
     .then((Events) => {
       res.json(Events);
@@ -17,22 +16,41 @@ router.get("/", (req, res) => {
         message: "Failed to retrieve all Events.",
       });
     });
-  
 });
 
 router.post("/", (req, res) => {
-  db.Event.create(req.body)
-    .then((NewEvent) => {
-      res.json(NewEvent);
-    })
-    .catch((err) => {
+  console.log(req.headers)
+  if (!req.headers.authorization) {
+    return res.status(401).json({
+      error: true,
+      data: null,
+      message: "Unauthorized",
+    });
+  }
+  jwt.verify(req.headers.authorization, process.env.SECRET, (err, decoded) => {
+    if (err) {
       console.log(err);
-      res.status(500).json({
+      return res.status(401).json({
         error: true,
         data: null,
-        message: "Failed to add new event.",
+        message: "Invalid token.",
       });
-    });
+    } else {
+      console.log({...req.body, hostID: decoded.userId});
+      db.Event.create({...req.body, hostID: decoded.userId})
+        .then((NewEvent) => {
+          res.json(NewEvent);
+        })
+        .catch((err) => {
+          console.log(err);
+          res.status(500).json({
+            error: true,
+            data: null,
+            message: "Failed to add new event.",
+          });
+        });
+    }
+  });
 });
 
 module.exports = router;
