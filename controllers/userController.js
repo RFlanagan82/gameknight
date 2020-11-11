cb2d2bef283755cbee9c0f687d3e0beba4a69851
ACE <67081507+aimeecesler@users.noreply.github.com>
2020-11-11 13:57:42 -0500

    added modal to update user

--- controllers/userController.js
+++ controllers/userController.js
@@ -38,5 +38,39 @@ router.get("/dashboard", (req, res) => {
   });
 });
 // EDIT USER - MAKE CHANGES TO LOGGED IN USER
+router.put("/", (req, res) => {
+  if (!req.headers.authorization) {
+    return res.status(401).json({
+      error: true,
+      data: null,
+      message: "Unauthorized",
+    });
+  }
+  jwt.verify(req.headers.authorization, process.env.SECRET, (err, decoded) => {
+    if (err) {
+      console.log(err);
+      return res.status(401).json({
+        error: true,
+        data: null,
+        message: "Invalid token.",
+      });
+    } else {
+      console.log(decoded);
+      db.User.findByIdAndUpdate(decoded.userId, req.body)
+        .then((user) => {
+          res.json(user);
+        })
+        .catch((err) => {
+          console.log(err);
+          res.status(500).json({
+            error: true,
+            data: null,
+            message: "Could not update user.",
+          });
+        });
+    }
+  });
+});
+
 
 module.exports = router;
