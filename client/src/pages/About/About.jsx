1a3f18a21c48f48b76fdb3269898751a20c472e4
Ryan Flanagan <flanagan.ryan@gmail.com>
2020-11-11 15:45:20 -0500

    created linen background color on About page

--- client/src/pages/About/About.jsx
+++ client/src/pages/About/About.jsx
@@ -1,19 +1,22 @@
-import React from 'react';
+import React from "react";
 import teamjson from "./team.json";
 import Aboutcard from "./Aboutcard";
+import ContainerFluid from "../../components/ContainerFluid/ContainerFluid";
 import Container from "../../components/Container/Container";
 import Row from "../../components/Row/Row";
 
 const About = () => {
-    return (
-        <Container>
-           <Row>
-            {teamjson.map(card => (
-                <Aboutcard {...card} />
-            ))}
-           </Row> 
-        </Container>
-    );
+  return (
+    <ContainerFluid className="aboutContainerFluid">
+      <Container className="aboutContainer">
+        <Row>
+          {teamjson.map((card) => (
+            <Aboutcard {...card} />
+          ))}
+        </Row>
+      </Container>
+    </ContainerFluid>
+  );
 };
 
 export default About;
